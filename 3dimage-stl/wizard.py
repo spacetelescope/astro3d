f4bc26106a00bd3d220de6552945519ff439bd5f
Roshan <rrao1394@gmail.com>
2014-08-19 14:25:34 -0400

    added comments

--- 3dimage-stl/wizard.py
+++ 3dimage-stl/wizard.py
@@ -4,7 +4,37 @@ from astroObjects import *
 
 class ThreeDModelWizard(QWizard):
 
+	"""
+	The ThreeDModelWizard is a subclass of QWizard and is meant to guide users through the creation 
+	of a 3D STL file from a 2D image. It contains seven pages, whose functions are explained below:
+	1. Introductory Page:
+		This page gives an explanation of the purpose of the wizard.
+	2. Image Load Page:
+		This page allows the user to load a .fits image.
+	3. Image Resize Page:
+		This page allows the user to resize the image to appropriate dimensions.
+	4. Intensity Scale Page:
+		This page allows the user to view the image after linear, logarithmic, or square root filters 
+		have been applied.
+	5. Region Page:
+		This page allows the user to draw and save regions. It also allows hiding (and showing) of 
+		regions, along with the ability to merge and split regions. Note: Merging and splitting do not 
+		work at this time.
+	6. Identify Peak Page:
+		This page allows the user to save the 15 brightest star clusters to be marked in the model.
+	7. Make Model Page:
+		This page allows the user to construct and save the model as an STL file.
+	"""
+
 	def __init__(self, parent, debug=False):
+		"""
+		Inputs:
+			parent - the instantiating widget, in this case astroVisual.AstroGUI.
+			debug - if debug is True, then only the final page is added to the display, as all other 
+					pages' functions are handled by AstroGUI.run_auto_login_script().
+		Variables:
+			self.parent - same as input parent
+		"""
 		super(ThreeDModelWizard, self).__init__(parent)
 		self.parent = parent
 		if not debug:
@@ -19,6 +49,7 @@ class ThreeDModelWizard(QWizard):
 		self.setVisible(True)
 
 	def createIntroPage(self):
+		"""Creates and returns the introductory page."""
 		page = QWizardPage()
 		page.setTitle("Introduction")
 
@@ -31,6 +62,7 @@ class ThreeDModelWizard(QWizard):
 		return page
 
 	def createImageLoadPage(self):
+		"""Creates a QWizardPage that allows the user to load an image by calling AstroGUI.fileLoad."""
 		page = QWizardPage()
 		page.setTitle("Load an Image")
 		label = QLabel("First, click the button to load an image of a galaxy:")
@@ -44,7 +76,21 @@ class ThreeDModelWizard(QWizard):
 
 	class IntensityScalePage(QWizardPage):
 
+		"""
+		A subclass of QWizardPage. Contains three checkboxes for choosing between a linear, 
+		logarithmic, or square root filter.
+		"""
+
 		def __init__(self, grandparent):
+			"""
+			Inputs:
+				grandparent - the instantiating class's parent, in this case AstroGUI.
+			Variables:
+				self.grandparent - same as input grandparent
+				self.bgroup - the QButtonGroup containing the three checkboxes. It ensures that the 
+								boxes are exclusive and sets an id for each box so it knows which one 
+								is checked.
+			"""
 			super(ThreeDModelWizard.IntensityScalePage, self).__init__()
 			self.grandparent = grandparent
 
@@ -77,6 +123,10 @@ class ThreeDModelWizard(QWizard):
 			self.setLayout(button_grid)
 
 		def apply(self):
+			"""
+			Called when user clicks the apply button. Gets the id of the checked box from self.bgroup 
+			then calls AstroGUI.setTransformation with the appropriate argument.
+			"""
 			_id = self.bgroup.checkedId()
 			if _id == 0:
 				self.grandparent.setTransformation('Linear')
@@ -87,7 +137,52 @@ class ThreeDModelWizard(QWizard):
 
 	class ImageResizePage(QWizardPage):
 
+		"""
+		A subclass of QWizardPage. Allows the user to change the image dimensions. Contains an input 
+		box for image width, while automatically generating the correct height for the image aspect 
+		ratio.
+
+		This page is important because the image must be of a specific size. Aside from the slowdown 
+		of the engine as it processes a very high-res image, the actual MakerBot software cannot 
+		handle more than ~2 million triangles. The full size model is printed front and back and 
+		split into two halves, meaning that a 1000x1000 image will create a 4 million triangle model, 
+		which will subsequently produce two 2 million triangle halves. Since part of the image is 
+		cropped during the model creation process, the average image can probably be most 1300x1300.
+
+		The restriction against smaller image sizes stems from the need to provide texture. If there 
+		are too few pixels, then the dots and lines making up the texture will be spaced too far apart, 
+		and will therefore be very course.
+
+		Exceptions:
+			If texture is unnecessary, then a much lower resolution may be used (down to 500x500 at
+			least). It is important to note than when printing smaller models (models that are not 
+			split into two halves), that a lower resolution is required (the smaller model will still
+			be ~4 million triangles), and therefore texture cannot be added. However, the engine is 
+			currently optimized for a 1000x1000 pixel image, and will therefore require some 
+			adjustment before a 500x500 pixel image can be run through.
+
+			If the image is rotated, then the crop function of the engine will remove a far greater 
+			amount of the image. It has been my experience that a rotated image with dimensions 
+			2000x2000 will be cropped to an image just slightly over 1000x1000, which is perfect for 
+			this project. If we don't want users to have to deal with this issue, it may be possible 
+			to resize all images to 2000x2000 here, then resize the image again after the crop.
+		"""
+
 		def __init__(self, grandparent):
+			"""
+			Inputs:
+				grandparent - the instantiating class's parent, in this case AstroGUI.
+			Variables:
+				self.grandparent - same as input grandparent
+				self.height, self.width - the current height/width of the loaded image array.
+											Note: these are instantiated as 0, but are set to the 
+											actual height/width when the page is intialized.
+				self.messageLabel - A QLabel. Informs the user whether the image needs to be increased 
+									or decreased in size.
+				self.xtext - a QLineEdit. Allows the user to input the new desired width for the image.
+				self.ylabel - a QLabel. Displays the appropriate height of the image given the user 
+								input for width in order to maintain the aspect ratio.
+			"""
 			super(ThreeDModelWizard.ImageResizePage, self).__init__()
 			self.grandparent = grandparent
 			self.setTitle("Adjust Image Resolution")
@@ -112,6 +207,11 @@ class ThreeDModelWizard(QWizard):
 			self.setLayout(vbox)
 
 		def resetUI(self, height, width):
+			"""
+			Input: int height, int width
+			Purpose: Creates the user interface, showing the appropriate message label along width the
+						 correct height/width.
+			"""
 			self.height = height
 			self.width = width
 			numpixels = self.height * self.width
@@ -132,7 +232,10 @@ class ThreeDModelWizard(QWizard):
 			self.xtext.setText(str(width))
 
 		def setYText(self):
-			# Y size always equals x size...
+			"""
+			Called whenever self.xtext is edited. Automatically changes self.ylabel to match the 
+			correct aspect ratio. Also changes self.width and self.height.
+			"""
 			width = self.xtext.text()
 			if not width.isEmpty():
 				width = int(width)
@@ -142,9 +245,19 @@ class ThreeDModelWizard(QWizard):
 				self.ylabel.setText(" x " + str(int(self.height)))
 
 		def changeSize(self):
+			"""
+			Called when the resize button is clicked. Calls AstroGUI.resizeImage with the input 
+			height and width.
+			"""
 			self.grandparent.resizeImage(self.grandparent.curr, int(self.width), int(self.height))
 
 		def initializePage(self):
+			"""
+			Called right before the page is displayed. Since this page is created before the image is 
+			loaded, there is no height/width to obtain at the beginning. However, if the user follows 
+			the proper order then there will be at the time the page is viewed, and so this method can 
+			obtain the correct height/width.
+			"""
 			super(ThreeDModelWizard.ImageResizePage, self).initializePage()
 			h, w = self.grandparent.curr.data.shape
 			self.resetUI(h, w)
