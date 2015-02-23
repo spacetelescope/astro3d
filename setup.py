37dd0089b730221c9b8ec36f9dabd10824a927b7
Pey Lian Lim <lim@stsci.edu>
2015-02-23 13:58:54 -0500

    Fixed version num in setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = 'astro3d',
-    version = '0.2.0.dev',
+    version = '0.3.0.dev',
     description = 'Hubble Image 3D Printing',
     author = 'STScI',
     author_email = 'help@stsci.edu',
