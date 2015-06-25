8ff9153d8bd02cc94be218a3290b27788e7b1172
Larry Bradley <larry.bradley@gmail.com>
2015-06-25 11:29:11 -0400

    Remove flipud in to_mesh

--- astro3d/utils/meshcreator.py
+++ astro3d/utils/meshcreator.py
@@ -1,17 +1,17 @@
-"""Functions to convert image array to STL files."""
-from __future__ import division, print_function
-
-# STDLIB
+"""
+This module provides functions to convert an image array to STL files.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 from copy import deepcopy
 from struct import unpack
-
-# Anaconda
 import numpy as np
 from astropy import log
 
 
 def get_triangles(npimage, depth=10):
-    """Make upper left and lower right triangles.
+    """
+    Make upper left and lower right triangles.
 
     Automatically exclude invalid triangles (triangles with
     one vertex off the edge).
@@ -26,8 +26,8 @@ def get_triangles(npimage, depth=10):
     -------
     normalized_triset : ndarray
         An array of triangles.
-
     """
+
     npimage = npimage + depth
     h, w = npimage.shape
     y, x = np.indices((h, w))
@@ -52,6 +52,7 @@ def get_triangles(npimage, depth=10):
 
 def make_sides(ults, lrts):
     """Creates the sides of the base."""
+
     a = ults[0].copy()
     a[:,:,3] = a[:,:,1]
     a[:,2,3] = 0
@@ -96,6 +97,7 @@ def make_sides(ults, lrts):
 
 def make_bottom(height, width):
     """Create the bottom of the base."""
+
     bottom = np.array([[[width, 0, 0], [0, 0, 0], [0, height, 0]],
                        [[width, height, 0], [width, 0, 0], [0, height, 0]]])
     triset = np.zeros((2, 4, 3))
@@ -111,7 +113,8 @@ def make_bottom(height, width):
 
 
 def normalize_triangles(triset):
-    """Ensure model can fit on MakerBot plate.
+    """
+    Ensure model can fit on MakerBot plate.
 
     .. note::
 
@@ -119,8 +122,8 @@ def normalize_triangles(triset):
         or a different printer.
 
         All sizes are in mm, not inches.
-
     """
+
     xsize = triset[:,1:,0].ptp()
     if xsize > 140:
         triset = triset * 140 / float(xsize)
@@ -137,10 +140,12 @@ def normalize_triangles(triset):
 
 
 def get_cross(triset):
-    """Set the normal vector for each triangle.
-    This is necessary for some 3D printing software, including MakerWare.
-
     """
+    Set the normal vector for each triangle.
+
+    This is necessary for some 3D printing software, including MakerWare.
+    """
+
     t1 = triset[:,:,1]
     t2 = triset[:,:,2]
     t3 = triset[:,:,3]
@@ -151,9 +156,10 @@ def get_cross(triset):
     return triset
 
 
-def to_mesh(image, filename, depth=1, double=False, _ascii=False):
-    """Write an image to STL file by splitting each pixel
-    into two triangles.
+def to_mesh(image, filename, depth=1, double_sided=False, _ascii=False):
+    """
+    Write an image to STL file by splitting each pixel into two
+    triangles.
 
     Parameters
     ----------
@@ -170,26 +176,23 @@ def to_mesh(image, filename, depth=1, double=False, _ascii=False):
         For writing JPG or PNG images, a depth of 10 probably
         suffices.
 
-    double : bool
-        Single- or double-sided.
+    double_sided : bool
+        Set to `True` for a double-sided model.
 
     _ascii : bool
         Write in binary or ASCII format.
         Binary STL file is harder to debug, but takes up less
         storage space.
-
     """
+
     if isinstance(image, np.ma.core.MaskedArray):
         npimage = deepcopy(image.data)
     else:
         npimage = deepcopy(image)
 
-    # Flip it to make it easier to compare GUI display with STL viewer
-    npimage = np.flipud(npimage)
-
     triset = get_triangles(npimage, depth)
 
-    if double:
+    if double_sided:
         triset2 = triset.copy()
         triset2[:,0] = -triset2[:,0]
         triset2[:,1:,2] = -triset2[:,1:,2]
@@ -206,7 +209,8 @@ def to_mesh(image, filename, depth=1, double=False, _ascii=False):
 
 
 def write_binary(triset, filename):
-    """Write a binary STL file.
+    """
+    Write a binary STL file.
 
     Parameters
     ----------
@@ -215,8 +219,8 @@ def write_binary(triset, filename):
 
     filename : str
         Output filename.
-
     """
+
     triset = triset.astype('<f4')
     triset = triset.reshape((triset.shape[0], 12))
     buff = np.zeros((triset.shape[0],), dtype=('f4,'*12+'i2'))
@@ -239,13 +243,14 @@ def write_binary(triset, filename):
 
 
 def write_ascii(triset, filename):
-    """Like :func:`write_binary` but in ASCII format.
+    """
+    Like :func:`write_binary` but in ASCII format.
 
     .. note::
 
         Recommended for debugging only.
-
     """
+
     with open(filename, 'w') as f:
         f.write("solid bozo\n")
         for t in triset:
@@ -260,7 +265,8 @@ def write_ascii(triset, filename):
 
 
 def read_binary(filename):
-    """Read binary STL file.
+    """
+    Read binary STL file.
 
     http://sukhbinder.wordpress.com/2013/11/28/binary-stl-file-reader-in-python-powered-by-numpy/
 
@@ -271,8 +277,8 @@ def read_binary(filename):
     Returns
     -------
     header, normals, points, v1, v2, v3
-
     """
+
     with open(filename, 'rb') as fp:
         header = fp.read(80)
         nn = fp.read(4)
