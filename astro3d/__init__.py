0f2f711aaf3aa1faccf172b7958bc619a29f96e4
Larry Bradley <larry.bradley@gmail.com>
2015-09-02 10:17:08 -0400

    Use astropy package template

--- astro3d/__init__.py
+++ astro3d/__init__.py
@@ -1,1 +1,14 @@
-from .model3d import *
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+A package to create a 3D model from an astronomical image.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from .model3d import *
