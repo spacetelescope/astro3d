a9f512bd7c820dd85d7e518d58bf308633492573
J.D.Eisenhamer <eisenhamer@stsci.edu>
2016-03-21 12:13:25 -0400

    Paint a path
    WIP #94

--- astro3d/gui/qt4/shape_editor.py
+++ astro3d/gui/qt4/shape_editor.py
@@ -13,12 +13,12 @@ from .. import signaldb
 __all__ = ['ShapeEditor']
 
 
-VALID_KINDS = set((
-    'freepolygon', 'freepath',
+VALID_KINDS = [
+    'freepolygon', 'paint',
     'circle', 'rectangle',
     'triangle', 'righttriangle',
     'square', 'ellipse', 'box'
-))
+]
 
 INSTRUCTIONS = (
     'Draw a region with the cursor. '
@@ -70,17 +70,23 @@ class ShapeEditor(QtGui.QWidget):
         self._canvas = canvas
 
         # Setup parameters
-        self.drawkinds = list(
-            VALID_KINDS.intersection(self.canvas.get_drawtypes())
-        )
-        self.drawkinds.sort()
+        self.drawkinds = VALID_KINDS
 
-        # Setup for actual drawing
-        canvas.enable_draw(True)
-        canvas.enable_edit(True)
+        # Create paint mode
+        canvas.add_draw_mode(
+            'paint',
+            down=self.paint_start,
+            move=self.paint_stroke,
+            up=self.paint_stop
+        )
+
+        # Setup common events.
         canvas.set_callback('draw-event', self.draw_cb)
         canvas.set_callback('edit-event', self.edit_cb)
         canvas.set_callback('edit-select', self.edit_select_cb)
+
+        # Initial canvas state
+        canvas.enable_edit(True)
         canvas.setSurface(self.surface)
         canvas.register_for_cursor_drawing(self.surface)
         canvas.set_draw_mode('edit')
@@ -104,12 +110,28 @@ class ShapeEditor(QtGui.QWidget):
         self.type_item = type_item
         if self.canvas is None:
             raise RuntimeError('Internal error: no canvas to draw on.')
-        self.set_drawparams_cb()
+        self.draw_region()
+
+    def draw_region(self):
+        """User draw a new region"""
+        kind = self.get_selected_kind()
+        if kind == 'paint':
+            self.paint_region()
+        else:
+            self.shape_region(kind=kind)
+
+    def shape_region(self, kind=None):
+        self.set_drawparams_cb(kind=kind)
+        self.canvas.enable_draw(True)
         self.canvas.set_draw_mode('draw')
 
-    def set_drawparams_cb(self):
+    def paint_region(self):
+        self.canvas.set_draw_mode('paint')
+
+    def set_drawparams_cb(self, kind=None):
         self.logger.debug('children="{}"'.format(self.children))
-        kind = self.drawkinds[self.children.draw_type.get_index()]
+        if kind is None:
+            kind = self.get_selected_kind()
         linewidth = self.children.line_width.get_value()
         params = {'linewidth': linewidth}
         try:
@@ -119,8 +141,8 @@ class ShapeEditor(QtGui.QWidget):
         self.canvas.set_drawtype(kind, **params)
 
     def draw_cb(self, canvas, tag):
-        """Draw callback"""
-        self.canvas.set_draw_mode('edit')
+        """Shape draw completion"""
+        self.canvas.enable_draw(False)
         shape = canvas.get_object_by_tag(tag)
         region_mask = partial(
             self.surface.get_shape_mask,
@@ -128,6 +150,7 @@ class ShapeEditor(QtGui.QWidget):
             shape
         )
         self.type_item.add_shape(shape=shape, mask=region_mask, id=tag)
+        self.canvas.set_draw_mode('edit')
 
     def edit_cb(self, *args, **kwargs):
         """Edit callback"""
@@ -152,6 +175,69 @@ class ShapeEditor(QtGui.QWidget):
         delta = float(w.get_text())
         self.canvas.edit_scale(delta, delta, self.surface)
         signaldb.ModelUpdate()
+
+    def new_brush(self, copy_from=None):
+        """Create a new brush shape"""
+        brush = self.canvas.get_draw_class('squarebox')(
+            x=0., y=0., radius=10,
+            linewidth=0,
+            fill=True, fillcolor='red'
+        )
+        if copy_from is not None:
+            brush.x = copy_from.x
+            brush.y = copy_from.y
+            brush.radius = copy_from.radius
+        self.canvas.add(brush)
+        return brush
+
+    def paint_start(self, canvas, event, data_x, data_y, surface):
+        """Start a paint stroke"""
+        self.logger.debug((
+            'canvas="{}" '
+            'event="{}" '
+            'x="{}" y="{}"'
+            'surface="{}"'
+        ).format(
+            canvas,
+            event,
+            data_x,
+            data_y,
+            surface
+        ))
+
+        self.brush = self.new_brush()
+        self.brush_move(data_x, data_y)
+
+    def paint_stroke(self, canvas, event, data_x, data_y, surface):
+        """Perform a paint stroke"""
+        previous = self.brush
+        self.brush = self.new_brush(previous)
+        self.brush_move(data_x, data_y)
+        self.stroke(previous, self.brush)
+
+    def paint_stop(self, canvas, event, data_x, data_y, surface):
+        """Finish paint stroke"""
+        self.paint_stroke(canvas, event, data_x, data_y, surface)
+        self.canvas.set_draw_mode('edit')
+
+    def stroke(self, previous, current):
+        """Stroke to current brush position"""
+        poly_points = get_bpoly(previous, current)
+        polygon = self.canvas.get_draw_class('polygon')(
+            poly_points,
+            linewidth=current.linewidth,
+            fill=current.fill,
+            fillcolor=current.fillcolor
+        )
+        self.canvas.add(polygon)
+
+    def get_selected_kind(self):
+        kind = self.drawkinds[self.children.draw_type.get_index()]
+        return kind
+
+    def brush_move(self, x, y):
+        self.brush.move_to(x, y)
+        self.canvas.update_canvas(whence=3)
 
     def _build_gui(self):
         """Build out the GUI"""
@@ -226,3 +312,51 @@ class ShapeEditor(QtGui.QWidget):
         layout.addWidget(edit_frame.get_widget(), stretch=0)
         layout.addWidget(spacer.get_widget(), stretch=1)
         self.setLayout(layout)
+
+
+def get_bpoly(box1, box2):
+    """Get the bounding polygon of two boxes"""
+    left = box1
+    right = box2
+    if box2.x < box1.x:
+        left = box2
+        right = box1
+    left_llx, left_lly, left_urx, left_ury = left.get_llur()
+    right_llx, right_lly, right_urx, right_ury = right.get_llur()
+
+    b = [(left_llx, left_lly)]
+    if left.y <= right.y:
+        b.extend([
+            (left_urx, left_lly),
+            (right_urx, right_lly),
+            (right_urx, right_ury),
+            (right_llx, right_ury),
+            (left_llx, left_ury)
+        ])
+    else:
+        b.extend([
+            (right_llx, right_lly),
+            (right_urx, right_lly),
+            (right_urx, right_ury),
+            (left_urx, left_ury),
+            (left_llx, left_ury)
+        ])
+    return b
+
+
+def corners(box):
+    """Get the corners of a box
+
+    Returns
+    -------
+    List of the corners starting at the
+    lower left, going counter-clockwise
+    """
+    xll, yll, xur, yur = box.get_llur()
+    corners = [
+        (xll, yll),
+        (xll, yur),
+        (xur, yur),
+        (xur, yll)
+    ]
+    return corners
