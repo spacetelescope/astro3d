f6efec5b0158eb91a68ee09acfe2c2d251b61bfa
Jonathan Eisenhamer <eisenham@inanna2.stsci.edu>
2019-01-23 17:19:16 -0500

    Add tests for Model3d.make

---
+++ astro3d/tests/test_model3d.py
@@ -0,0 +1,43 @@
+"""Test the core operations of model3d"""
+
+from filecmp import cmp
+from os import environ
+from pathlib import Path
+import pytest
+
+from astro3d.core.model3d import Model3D
+
+
+@pytest.mark.usefixtures('jail')
+@pytest.mark.skipif(
+    environ.get('ASTRO3D_TESTDATA') is None,
+    reason=(
+        'Test requires environmental ASTRO3D_TESTDATA'
+        ' pointing to the test data set.'
+    )
+)
+@pytest.mark.parametrize(
+    'id, make_kwargs',
+    [
+        ('full',      {'intensity': True,  'textures': True,  'double_sided': True,  'spiral_galaxy': True}),
+        ('nospiral',  {'intensity': True,  'textures': True,  'double_sided': True,  'spiral_galaxy': False}),
+        ('nodouble',  {'intensity': True,  'textures': True,  'double_sided': False, 'spiral_galaxy': False}),
+        ('intensity', {'intensity': True,  'textures': False, 'double_sided': False, 'spiral_galaxy': False}),
+        ('texture',   {'intensity': False, 'textures': True,  'double_sided': False, 'spiral_galaxy': False}),
+    ]
+)
+def test_make(id, make_kwargs):
+    """Test a full run of a spiral model"""
+
+    # Get the data
+    data_path = Path(environ['ASTRO3D_TESTDATA'])
+
+    model = Model3D.from_fits(data_path / 'ngc3344_crop.fits')
+    model.read_all_masks(str(data_path / 'features' / '*.fits'))
+
+    # Create and save the model
+    model.make(**make_kwargs)
+    model.write_stl('model3d', split_model=False)
+
+    # Check for file existence
+    assert cmp('model3d.stl', data_path / 'truth' / 'model3d_make' / id / 'model3d.stl')
