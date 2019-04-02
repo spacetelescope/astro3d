7dc6282baad286aa4afea85e9fac676a297a21ed
Jonathan Eisenhamer <eisenham@inanna.stsci.edu>
2019-04-02 17:24:10 -0400

    Update logging convenience functions and introduce debug commandline option

--- astro3d/app.py
+++ astro3d/app.py
@@ -11,8 +11,18 @@ from .gui.qt.process import MeshThread
 from .gui import (Controller, MainWindow, Model, signaldb)
 from .gui.start_ui_app import start_ui_app
 
+logger = make_logger(__name__)
+
 
 class Application(Controller):
+    """Start the astro3d Qt application
+
+    Parameters
+    ----------
+    argv: list or None
+        Argument list to pass to `argparse.ArgumentParser`.
+        If None, use `sys.argv`.
+    """
 
     # The UI event loop.
     ui_app = None
@@ -20,13 +30,14 @@ class Application(Controller):
     def __init__(self, argv=None):
 
         self.mesh_thread = None
-        self.logger = make_logger(name='astro3d', level=logging.DEBUG)
+        self.logger = logger
+
+        self.parse_command_line(argv)
+
         self._create_signals()
         self.model = Model(
             logger=self.logger
         )
-
-        self._argparse(argv)
 
         if self.__class__.ui_app is None:
             self.__class__.ui_app = start_ui_app(argv)
@@ -57,8 +68,27 @@ class Application(Controller):
     def process_finish(self, mesh, model3d):
         self.logger.debug('3D generation completed.')
 
-    def _argparse(self, argv):
-        parser = ArgumentParser()
+    def parse_command_line(self, argv):
+        """Parse command line arguments
+
+        Parameters
+        ----------
+        argv: list or None
+            Argument list to pass to `argparse.ArgumentParser`.
+            If None, use `sys.argv`.
+        """
+        parser = ArgumentParser(
+            description='astro3d: Create 3D models of astronomical images'
+        )
+        parser.add_argument(
+            '-D', '--debug',
+            help='Turn on debugging information',
+            action='store_true'
+        )
+        args = parser.parse_args(argv)
+
+        if args.debug:
+            self.logger.setLevel(logging.DEBUG)
 
     def _create_signals(self):
         signaldb.logger = self.logger
--- astro3d/util/logger.py
+++ astro3d/util/logger.py
@@ -1,20 +1,38 @@
+"""Logging convenience functions"""
 import logging
 
 
-def make_logger(name='__name__', level=logging.WARNING, format=None):
-    """Configure a basic logger"""
-    if format is None:
-        format = (
-            '%(asctime)s'
-            ' | %(levelname)1.1s'
-            ' %(filename)s:%(lineno)d (%(funcName)s)'
-            ' | %(message)s'
-        )
+# Default logging format
+DEFAULT_FORMAT = (
+    '%(asctime)s'
+    ' | %(levelname)1.1s'
+    ' %(filename)s:%(lineno)d (%(funcName)s)'
+    ' | %(message)s'
+)
+
+
+def make_logger(name, level=logging.INFO, log_format=None):
+    """Configure a basic logger
+
+    Parameters
+    ----------
+    name: str
+        The name of the logger. Usually `__name__` from the caller.
+
+    level: int
+        The logging level of the logger
+
+    log_format: str or None
+        The logging format
+    """
+
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    fmt = logging.Formatter(format)
-    stderrHdlr = logging.StreamHandler()
-    stderrHdlr.setFormatter(fmt)
-    logger.addHandler(stderrHdlr)
+
+    log_format = log_format or DEFAULT_FORMAT
+    formatter = logging.Formatter(log_format)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     return logger
